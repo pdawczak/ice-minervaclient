9f08e1b2019551a551c923546c58c2bd42eddcd7
Rob Hogan <rh389@cam.ac.uk>
2013-11-06 16:26:49 +0000

    Catch and log exceptions in MercuryListener (don't break the parent operation)

--- EventListener/MercuryListener.php
+++ EventListener/MercuryListener.php
@@ -7,6 +7,7 @@ use Ice\MercuryBundle\Event\OrderEvent;
 use Ice\MercuryBundle\Event\PaymentGroupEvent;
 use Ice\MinervaClientBundle\Entity\MinervaStatus;
 use Ice\MinervaClientBundle\Service\MinervaClient;
+use Symfony\Bridge\Monolog\Logger;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 class MercuryListener implements EventSubscriberInterface
@@ -14,11 +15,16 @@ class MercuryListener implements EventSubscriberInterface
     /** @var \Ice\MinervaClientBundle\Service\MinervaClient */
     private $minervaClient;
 
+    /** @var  Logger */
+    private $logger;
+
     /**
      * @param \Ice\MinervaClientBundle\Service\MinervaClient $minervaClient
+     * @param Logger $logger
      */
-    public function __construct(MinervaClient $minervaClient)
+    public function __construct(MinervaClient $minervaClient, $logger = null)
     {
+        $this->logger = $logger;
         $this->minervaClient = $minervaClient;
     }
 
@@ -40,15 +46,19 @@ class MercuryListener implements EventSubscriberInterface
     {
         $group = $event->getPaymentGroup();
 
-        if (0 === $group->getNetAmountUnallocated()) {
-            $this->minervaClient->setPaymentGroupPaymentStatusByReference($group->getExternalId(), MinervaStatus::PaymentBalanced);
-        } elseif (
-            $group->getNetAmountAllocated() > 0 &&
-            $group->getNetAmountAllocated() < $group->getNetAmount()
-        ) {
-            $this->minervaClient->setPaymentGroupPaymentStatusByReference($group->getExternalId(), MinervaStatus::PaymentPartPaid);
-        } elseif ($group->getNetAmountUnallocated() < 0) {
-            $this->minervaClient->setPaymentGroupPaymentStatusByReference($group->getExternalId(), MinervaStatus::PaymentOverpaid);
+        try {
+            if (0 === $group->getNetAmountUnallocated()) {
+                $this->minervaClient->setPaymentGroupPaymentStatusByReference($group->getExternalId(), MinervaStatus::PaymentBalanced);
+            } elseif (
+                $group->getNetAmountAllocated() > 0 &&
+                $group->getNetAmountAllocated() < $group->getNetAmount()
+            ) {
+                $this->minervaClient->setPaymentGroupPaymentStatusByReference($group->getExternalId(), MinervaStatus::PaymentPartPaid);
+            } elseif ($group->getNetAmountUnallocated() < 0) {
+                $this->minervaClient->setPaymentGroupPaymentStatusByReference($group->getExternalId(), MinervaStatus::PaymentOverpaid);
+            }
+        } catch (\Exception $e) {
+            $this->logException($e, "handle balance change of group ".$group->getId());
         }
     }
 
@@ -63,27 +73,31 @@ class MercuryListener implements EventSubscriberInterface
         $reference = $event->getOrder()->getReference();
 
         foreach ($suborders as $suborder) {
-            $group = $suborder->getPaymentGroup();
-            $numberOfSuborders = count($group->getSuborders());
+            try {
+                $group = $suborder->getPaymentGroup();
+                $numberOfSuborders = count($group->getSuborders());
 
-            if ($numberOfSuborders > 1) {
-                // Payment has already been made on this suborder.
-                // The new order is for an amendment so we don't need to update the payment status in Minerva
-                return;
-            }
+                if ($numberOfSuborders > 1) {
+                    // Payment has already been made on this suborder.
+                    // The new order is for an amendment so we don't need to update the payment status in Minerva
+                    return;
+                }
 
-            $username = $group->getAttributeByName('delegate_ice_id')->getValue();
-            $courseId = $group->getAttributeByName('course_id')->getValue();
-            $paymentMethod = $group->getAttributeByName('agreed_payment_method')->getValue();
+                $username = $group->getAttributeByName('delegate_ice_id')->getValue();
+                $courseId = $group->getAttributeByName('course_id')->getValue();
+                $paymentMethod = $group->getAttributeByName('agreed_payment_method')->getValue();
 
-            $this->minervaClient->setBookingOrderReference($username, $courseId, $reference);
+                $this->minervaClient->setBookingOrderReference($username, $courseId, $reference);
 
-            // It is assumed that invoices and student loans (and others, see WPMR-236) will be paid so a place is allocated.
-            // Other payment methods will be held for the appropriate amount of time and lost, unless payment is received.
-            if (in_array($paymentMethod, array('INVOICE', 'STUDENT_LOAN', 'CHEQUE', 'BACS', 'PDQ'))) {
-                $this->minervaClient->setPaymentGroupPaymentStatusByReference($group->getExternalId(), MinervaStatus::PaymentCommitted);
-            } else {
-                $this->minervaClient->setPaymentGroupPaymentStatusByReference($group->getExternalId(), MinervaStatus::PaymentArranged);
+                // It is assumed that invoices and student loans (and others, see WPMR-236) will be paid so a place is allocated.
+                // Other payment methods will be held for the appropriate amount of time and lost, unless payment is received.
+                if (in_array($paymentMethod, array('INVOICE', 'STUDENT_LOAN', 'CHEQUE', 'BACS', 'PDQ'))) {
+                    $this->minervaClient->setPaymentGroupPaymentStatusByReference($group->getExternalId(), MinervaStatus::PaymentCommitted);
+                } else {
+                    $this->minervaClient->setPaymentGroupPaymentStatusByReference($group->getExternalId(), MinervaStatus::PaymentArranged);
+                }
+            } catch (\Exception $e) {
+                $this->logException($e, "handle creation of suborder ".$suborder->getId());
             }
         }
     }
@@ -97,7 +111,11 @@ class MercuryListener implements EventSubscriberInterface
     {
         $network = $event->getOrderNetwork();
         foreach ($network->getPaymentGroupsInNetwork() as $paymentGroup) {
-            $this->minervaClient->setPaymentGroupPaymentStatusByReference($paymentGroup->getExternalId(), null);
+            try {
+                $this->minervaClient->setPaymentGroupPaymentStatusByReference($paymentGroup->getExternalId(), null);
+            } catch (\Exception $e) {
+                $this->logException($e, "handle deletion of payment group ".$paymentGroup->getExternalId());
+            }
         }
     }
 
@@ -111,5 +129,29 @@ class MercuryListener implements EventSubscriberInterface
             'mercury.post_create_order' => 'onCreateOrder',
             'mercury.delete_order' => 'onDeleteOrder',
         );
+    }
+
+    /**
+     * Logs information about an exception if a logger is available
+     *
+     * @param \Exception $e
+     * @param $intention
+     */
+    private function logException(\Exception $e, $intention)
+    {
+        if (!$this->logger) {
+            return;
+        }
+
+        $context = [
+            'message' => $e->getMessage()
+        ];
+
+        if ($e instanceof BadResponseException) {
+            $context['request'] = $e->getRequest()->__toString();
+            $context['response'] = $e->getResponse()->__toString();
+        }
+
+        $this->logger->err("Exception encountered when trying to ".$intention, $context);
     }
 }
